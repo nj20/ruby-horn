f8e927c88bceb9b9b5f989f70976a7a7731c735a
Naman <nj20@st-andrews.ac.uk>
2017-09-11 14:23:23 +0100

    add items after order is finished

--- app/src/main/java/none/rubyhorn/activity/WaitActivity.java
+++ app/src/main/java/none/rubyhorn/activity/WaitActivity.java
@@ -11,6 +11,10 @@ import android.view.View;
 
 import com.android.volley.Response;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import none.rubyhorn.R;
 import none.rubyhorn.models.Order;
 import none.rubyhorn.models.RestaurantMenu;
@@ -18,7 +22,7 @@ import none.rubyhorn.views.WaitView;
 
 public class WaitActivity extends AppCompatActivity
 {
-    public static Order order;
+    private static Order order;
     public static RestaurantMenu menu;
     public static String tableNumber;
 
@@ -50,4 +54,35 @@ public class WaitActivity extends AppCompatActivity
     {
     }
 
+    public static void addOrder(Order newOrder)
+    {
+        if(order == null)
+        {
+            setOrder();
+        }
+        order.totalPrice += newOrder.totalPrice;
+        order.totalQuantity += newOrder.totalQuantity;
+        Iterator it = newOrder.items.entrySet().iterator();
+        while (it.hasNext())
+        {
+            Map.Entry<String, Integer> pair = (Map.Entry)it.next();
+            String itemId = pair.getKey();
+            int quantity = pair.getValue();
+            if(order.items.containsKey(itemId))
+            {
+                order.items.put(itemId, order.items.get(itemId) + quantity);
+            }
+            else
+            {
+                order.items.put(itemId, quantity);
+            }
+            it.remove();
+        }
+    }
+
+    private static void setOrder()
+    {
+        order = new Order();
+        order.items = new HashMap<>();
+    }
 }
