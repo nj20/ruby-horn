e539b5ce2677537fa84f87b3a95087a2a3de3467
Naman <nj20@st-andrews.ac.uk>
2017-09-11 11:55:28 +0100

    Fixed waiting page

--- app/src/main/java/none/rubyhorn/activity/WaitActivity.java
+++ app/src/main/java/none/rubyhorn/activity/WaitActivity.java
@@ -1,9 +1,11 @@
 package none.rubyhorn.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuAdapter;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -32,10 +34,13 @@ public class WaitActivity extends AppCompatActivity
 
     private void setView()
     {
+        final AppCompatActivity instance = this;
         view = new WaitView(this, tableNumber, order, menu, new Response.Listener() {
             @Override
-            public void onResponse(Object response) {
-
+            public void onResponse(Object response)
+            {
+                Intent intent = new Intent(instance, MenuActivity.class);
+                startActivity(intent);
             }
         });
     }
